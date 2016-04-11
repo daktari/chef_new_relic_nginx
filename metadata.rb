83ad83e7e711db57fe171c2817ce1faa1aee483e
javier.heredia <javier.heredia@bq.com>
2016-04-11 10:14:45 +0200

    change metadata

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,4 @@ maintainer       'devopsBQ'
 license          'All rights reserved'
 description      'Configure nginx new-relic plugin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-depends			     ''
 version          '0.1.0'
